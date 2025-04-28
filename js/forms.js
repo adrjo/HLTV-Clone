748ecfc73e6843fe23bfac1feb426c8941c70ac0
adrjo <adrianjohansson16@gmail.com>
2025-04-28 15:47:15 +0200

    Darken background when displaying form and close it easier

--- js/forms.js
+++ js/forms.js
@@ -1,9 +1,15 @@
 export function displayForm(event) {
     const toast = document.getElementById("formed");
     toast.style = "display:flex";
+
+    const bg = document.getElementById("darken-bg");
+    bg.style = "display:block";
 }
 
 export function hideForm(event) {
     const toast = document.getElementById("formed");
     toast.style = "display:none";
+
+    const bg = document.getElementById("darken-bg");
+    bg.style = "display:none";
 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 import { displayForm, hideForm } from "./forms.js";
 import { submitPost } from "./posts.js";
 
-//New post
+// New post
 const newPostButton = document.getElementById("new_post");
 newPostButton.addEventListener("click", displayForm);
+
 
 const cancelButton = document.getElementById("cancel");
 cancelButton.addEventListener("click", hideForm);
 
+const bg = document.getElementById("darken-bg");
+bg.addEventListener("click", hideForm); // hide form when clicking anywhere other than the form too
+
 const submitButton = document.getElementById("submit_post");
 
 submitButton.addEventListener("click", submitPost);
+
+// load posts 
 
 
